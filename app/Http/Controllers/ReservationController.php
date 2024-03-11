a36019f4b239d7230668ce746ff17df04771277c
m0tay <mail.roi.douglas@gmail.com>
2024-03-11 16:02:44 +0000

    Reservation Logic

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreReservationRequest;
+use App\Http\Requests\UpdateReservationRequest;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+
+class ReservationController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    Auth::loginUsingId(3);
+
+    $this->authorize('viewAny', Reservation::class);
+
+    $reservations = Reservation::with('vehicle')->get();
+
+//        dd($reservations);
+
+    return Inertia::render('Reservations/Index', compact('reservations'));
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreReservationRequest $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Reservation $reservation)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Reservation $reservation)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateReservationRequest $request, Reservation $reservation)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Reservation $reservation)
+  {
+    //
+  }
+}
---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Reservation;
+use App\Models\User;
+use App\Models\Vehicle;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition()
+  {
+    $start_date = fake()->dateTimeBetween('2024-02-01', '2024-03-31');
+
+    return [
+      'start_date' => $start_date,
+      'end_date' => $start_date->modify('+1 hour'),
+      'rrule' => fake()->randomElement([null, 'RRULE:FREQ=WEEKLY;BYDAY=MO,WE,FR']), // Optional repeat rule
+      'status' => fake()->randomElement(['accepted', 'denied', 'done']),
+      'created_by' => fake()->randomElement(User::all()), // Generate a user and use the ID
+      'driver_id' => fake()->randomElement(User::all()), // Generate another user for the driver
+      'vehicle_id' => fake()->randomElement(Vehicle::all()), // Generate a vehicle
+      'description' => fake()->sentence(6), // Short description
+//      'previous_reservation' => fake()->boolean(20) ? Reservation::factory()->create()->id : null, // 20% chance of having a previous reservation
+    ];
+  }
+}
