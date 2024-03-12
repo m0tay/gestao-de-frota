db9c00e46ec3b13df9359ea49ed37f568a8a761c
m0tay <mail.roi.douglas@gmail.com>
2024-03-12 10:40:02 +0000

    corrected `reservations` model relationships

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -6,15 +6,37 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ *
+ */
 class Reservation extends Model
 {
   use HasFactory;
 
 
   // Reservation Model
+
+  /**
+   * @return BelongsTo
+   */
   public function vehicle(): BelongsTo
   {
-    return $this->belongsTo(Vehicle::class);
+    return $this->belongsTo(Vehicle::class, 'vehicle_id');
   }
 
+  /**
+   * @return BelongsTo
+   */
+  public function driver(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'driver_id');
+  }
+
+  /**
+   * @return BelongsTo
+   */
+  public function creator(): BelongsTo
+  {
+    return $this->belongsTo(User::class, 'created_by');
+  }
 }
