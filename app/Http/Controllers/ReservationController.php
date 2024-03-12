695d4277c5784708d21586f0552b3cce66af5ffb
m0tay <mail.roi.douglas@gmail.com>
2024-03-12 10:40:18 +0000

    doc'ed a bit

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreReservationRequest;
 use App\Http\Requests\UpdateReservationRequest;
+use App\Http\Resources\ReservationResource;
 use App\Models\Reservation;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -11,17 +12,17 @@ use Inertia\Inertia;
 class ReservationController extends Controller
 {
   /**
-   * Display a listing of the resource.
+   * Displays a list of reservations.
+   *
+   * @return \Inertia\Response  Renders the Inertia 'Reservations/Index' view with reservations data.
    */
   public function index()
   {
-    Auth::loginUsingId(3);
+    Auth::loginUsingId(3); // Temporary login for development
 
-    $this->authorize('viewAny', Reservation::class);
+    $this->authorize('viewAny', Reservation::class); // Enforce authorization policy
 
-    $reservations = Reservation::with('vehicle')->get();
-
-//        dd($reservations);
+    $reservations = Reservation::with('vehicle')->get(); // Fetch reservations with eager loading
 
     return Inertia::render('Reservations/Index', compact('reservations'));
   }
@@ -43,11 +44,20 @@ class ReservationController extends Controller
   }
 
   /**
-   * Display the specified resource.
+   * Displays detailed information about a specific reservation.
+   *
+   * @param  \App\Models\Reservation $reservation  The reservation to be displayed
+   * @return \Inertia\Response  Renders the Inertia 'Reservations/Show' view with a transformed reservation
    */
   public function show(Reservation $reservation)
   {
-    //
+    $reservation->load('driver', 'vehicle', 'creator'); // Load related models
+
+    $reservationResource = new ReservationResource($reservation); // Create a Laravel resource
+
+    return Inertia::render('Reservations/Show', [
+      'reservation' => $reservationResource->toArray(request())
+    ]);
   }
 
   /**
