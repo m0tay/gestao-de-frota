565dc75257de2dad7f0fed1faa2cc07a52594fe5
m0tay <mail.roi.douglas@gmail.com>
2024-03-13 16:03:54 +0000

    impletemented shadcn-vue components

---
+++ resources/js/Components/DateTimeInput.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {format} from 'date-fns'
+import {Calendar as CalendarIcon} from 'lucide-vue-next'
+
+import {onMounted, ref} from 'vue'
+import {cn} from '@/lib/utils'
+import {Button} from '@/Components/ui/button'
+import {Calendar} from '@/Components/ui/calendar'
+import {Popover, PopoverContent, PopoverTrigger,} from '@/Components/ui/popover'
+
+const date = defineModel({
+  type: Date,
+  required: true,
+});
+
+// const date = ref()
+
+</script>
+
+<template>
+  <Popover>
+    <PopoverTrigger as-child>
+      <Button
+        :variant="'outline'"
+        :class="cn(
+          'w-[280px] justify-start text-left font-normal bg-gray-50',
+          !date && 'text-muted-foreground',
+        )"
+      >
+        <CalendarIcon class="mr-2 h-4 w-4" />
+        <span>{{ date ? format(date, 'd MMM hh:mm') : "Escolha uma data e hora" }}</span>
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent class="w-auto p-0">
+      <Calendar v-model="date" mode="datetime"/>
+    </PopoverContent>
+  </Popover>
+</template>
---
+++ resources/js/Components/SelectInput.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/Components/ui/select'
+
+defineProps({
+  placeholder: '',
+  list: Object,
+})
+
+const selectedItem = defineModel({
+  type: Object,
+  required: true,
+});
+
+const updateSelectedItem = (newValue) => {
+  selectedItem.value = newValue;
+};
+</script>
+
+<template>
+  <Select v-model="selectedItem" @update:modelValue="updateSelectedItem">
+    <SelectTrigger class="bg-gray-50">
+      <SelectValue   :placeholder/>
+    </SelectTrigger>
+    <SelectContent>
+      <SelectGroup v-for="item in list" :key="item.id">
+        <SelectItem :value="item.id">
+          <span v-if="item.plate">{{ item.plate.toUpperCase() }}</span>
+          <span v-if="item.name">{{ item.name }}</span>
+        </SelectItem>
+      </SelectGroup>
+    </SelectContent>
+  </Select>
+</template>
