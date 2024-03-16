668c52b0ce69613e4c8886e765cc498600cfd9d7
m0tay <mail.roi.douglas@gmail.com>
2024-03-16 20:40:23 +0000

    fixed UI

--- resources/js/Pages/Reservations/Partials/ViewReservation.vue
+++ resources/js/Pages/Reservations/Partials/ViewReservation.vue
@@ -68,23 +68,9 @@ onBeforeUpdate(() => {
     <Modal :show="props.show" @close="$emit('close')">
         <section class="space-y-6 p-8 w-full" @contextmenu.prevent>
             <header>
-                <section class="flex flex-col lg:flex-row justify-between">
+                <section class="flex flex-col lg:flex-row justify-between gap-y-6">
                     <div>
                         <h2 class="text-3xl text-center xl:text-4xl font-bold text-gray-900">Sobre esta Requisição</h2>
-                        <small>Agendado por {{props.selectedEvent.creator.name}}</small>
-                        <!--            <div class="flex items-start gap-x-2 text-muted-foreground">-->
-                        <!--              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"-->
-                        <!--                   stroke="currentColor" class="w-6 h-6">-->
-                        <!--                <path stroke-linecap="round" stroke-linejoin="round"-->
-                        <!--                      d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"/>-->
-                        <!--              </svg>-->
-                        <!--              <small v-if="props.selectedEvent.status === 'rescheduled'" class="lg:my-auto">Esta requisição foi-->
-                        <!--                reagendada.</small>-->
-                        <!--              <small v-if="props.selectedEvent.status === 'denied'" class="lg:my-auto">Esta requisição foi-->
-                        <!--                cancelada.</small>-->
-                        <!--              <small v-if="props.selectedEvent.status === 'done'" class="lg:my-auto">Esta requisição está-->
-                        <!--                concluída.</small>-->
-                        <!--            </div>-->
                     </div>
                     <ReservationStatus :reservation="props.selectedEvent"/>
 
@@ -110,10 +96,19 @@ onBeforeUpdate(() => {
                 </div>
             </div>
 
-            <div class="mt-6 max-w-full">
+            <div v-show="props.selectedEvent.creator.id !== props.selectedEvent.driver.id" class="mt-6 max-w-full">
+                <InputLabel value="Agendado por" for="creator"/>
+                <FakeSelectInput disabled id="creator" :placeholder="props.selectedEvent.creator.name"/>
+            </div>
+
+            <div v-show="props.selectedEvent.creator.id !== props.selectedEvent.driver.id" class="mt-6 max-w-full">
                 <InputLabel value="Condutor" for="driver"/>
                 <FakeSelectInput disabled id="driver" :placeholder="props.selectedEvent.driver.name"/>
+            </div>
 
+            <div v-show="props.selectedEvent.creator.id === props.selectedEvent.driver.id" class="mt-6 max-w-full">
+                <InputLabel value="Agendado pelo condutor" for="driver"/>
+                <FakeSelectInput disabled id="driver" :placeholder="props.selectedEvent.driver.name"/>
             </div>
 
             <div class="mt-6 max-w-full">
