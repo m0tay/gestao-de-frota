0fc5bf445fc2aad8e94f264899154dfeddec2164
m0tay <mail.roi.douglas@gmail.com>
2024-03-18 13:33:10 +0000

    fixed HOME route to `/agenda`

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/reservations';
+    public const HOME = '/agenda';
     public const DASHBOARD = '/dashboard';
 
     /**
