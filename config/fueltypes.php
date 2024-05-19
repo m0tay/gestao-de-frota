99198f1646e6525e7d8ed26427b2a9f609f3e109
m0tay <mail.roi.douglas@gmail.com>
2024-05-19 16:00:25 +0100

    adding new way to set fueltypes

---
+++ config/fueltypes.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'assigning' => [
+        'gasoline' => [
+            'name' => 'Gasolina',
+        ],
+        'gasoleo' => [
+            'name' => 'Gasóleo',
+        ],
+        'hybrid-gasoline' => [
+            'name' => 'Híbrido (Gasolina)',
+        ],
+        'hybrid-gasoleo' => [
+            'name' => 'Híbrido (Gasóleo)',
+        ],
+    ],
+    'refuelling' => [
+        'adblue' => [
+            'name' => 'AdBlue',
+        ],
+        'gasoline' => [
+            'name' => 'Gasolina',
+        ],
+        'gasoline-premium' => [
+            'name' => 'Gasolina Premium',
+        ],
+        'gasoleo' => [
+            'name' => 'Gasóleo',
+        ],
+        'gasoleo-premium' => [
+            'name' => 'Gasóleo Premium',
+        ],
+    ],
+];
