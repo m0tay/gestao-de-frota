11b5090827f2ab19caf262c3039f3f9b2842435a
m0tay <mail.roi.douglas@gmail.com>
2024-05-25 19:06:15 +0100

    test: assert that user can see reservations on agenda

--- tests/Feature/Http/Controllers/ReservationControllerTest.php
+++ tests/Feature/Http/Controllers/ReservationControllerTest.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Reservation;
+
+beforeEach(function () {
+    Role::create(['name' => 'user']);
+});
+
 test('example', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
+});
+
+test('assert that user can see reservations on agenda', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user)
+        ->get('/agenda')
+        ->assertSee('Reservations');
 });
