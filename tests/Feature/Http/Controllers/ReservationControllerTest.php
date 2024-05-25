314ef1ab0c4248fc83b6cefa8e237d1932c7d374
m0tay <mail.roi.douglas@gmail.com>
2024-05-25 22:25:06 +0100

    test: assert that user can create a reservation

--- tests/Feature/Http/Controllers/ReservationControllerTest.php
+++ tests/Feature/Http/Controllers/ReservationControllerTest.php
@@ -3,6 +3,8 @@
 use App\Models\User;
 use App\Models\Role;
 use App\Models\Reservation;
+use App\Models\Vehicle;
+use Carbon\Carbon;
 
 beforeEach(function () {
     Role::create(['name' => 'user']);
@@ -20,3 +22,40 @@ test('assert that user can see reservations on agenda', function () {
         ->get('/agenda')
         ->assertSee('Reservations');
 });
+
+test('assert that user can create a reservation', function () {
+    $driver = User::factory()->create(['name' => 'José', 'email' => 'manager@manager.com']);
+    $vehicle = Vehicle::factory()->create();
+    $creator = $driver;
+
+    $response = $this->actingAs($driver)
+        ->post('/agenda', [
+            'driver' => [
+                'id' => $driver->id,
+                'name' => $driver->name,
+                'email' => $driver->email,
+            ],
+            'vehicle' => [
+                'id' => $vehicle->id,
+            ],
+            'creator' => [
+                'id' => $driver->id,
+                'name' => $driver->name,
+                'email' => $driver->email,
+                'role' => [
+                    'id' => 2,
+                    'name' => 'manager',
+                ],
+                'company' => 'roboplan',
+            ],
+            'description' => 'simss',
+            'start' => '2024-05-25T22:10:00.161Z',
+            'end' => '2024-05-25T23:10:00.161Z',
+        ]);
+
+    $response->assertStatus(302);
+
+    $response->assertRedirect('/');
+    
+});
+
