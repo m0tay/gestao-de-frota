29fef380032a8ca4cfb908ca2bfbc470d65feb63
m0tay <mail.roi.douglas@gmail.com>
2024-05-26 22:22:57 +0100

    feat: adding validation request for vehicle store request

--- app/Http/Requests/StoreVehicleRequest.php
+++ app/Http/Requests/StoreVehicleRequest.php
@@ -14,7 +14,16 @@ class StoreVehicleRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'plate' => ['required', 'string', 'regex:(^(?:[A-Z]{2}-\d{2}-\d{2})|(?:\d{2}-[A-Z]{2}-\d{2})|(?:\d{2}-\d{2}-[A-Z]{2})|(?:[A-Z]{2}-\d{2}-[A-Z]{2})$)'],
+            'brand' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.brands')))],
+            'model' => ['required', 'string'],
+            'group' => ['required', 'string'],
+            'category' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.categories')))],
+            'proprietary' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.proprietaries')))],
+            'status' => ['required', 'string', 'in:active'],
+            'kms' => ['required', 'integer'],
+            'fuel_type' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.fueltypes.assigning')))],
         ];
     }
 }
+
