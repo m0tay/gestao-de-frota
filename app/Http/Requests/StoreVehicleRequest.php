2f0a39170050d9f5493f64770c20baa479233dc6
m0tay <mail.roi.douglas@gmail.com>
2024-05-27 15:21:39 +0100

    wip: added new fields and fixed imploding

--- app/Http/Requests/StoreVehicleRequest.php
+++ app/Http/Requests/StoreVehicleRequest.php
@@ -15,15 +15,25 @@ class StoreVehicleRequest extends FormRequest
     {
         return [
             'plate' => ['required', 'string', 'regex:(^(?:[A-Z]{2}-\d{2}-\d{2})|(?:\d{2}-[A-Z]{2}-\d{2})|(?:\d{2}-\d{2}-[A-Z]{2})|(?:[A-Z]{2}-\d{2}-[A-Z]{2})$)'],
-            'brand' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.brands')))],
+            'brand' => ['required', 'string', 'in:' . implode(',', config('vehicles.brands'))],
             'model' => ['required', 'string'],
-            // TODO 'group' => ['required', 'string'],
-            'category' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.categories')))],
-            'proprietary' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.proprietaries')))],
-            'status' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.statuses')))],
+            'color' => ['required', 'string'],
+            'category' => ['required', 'string', 'in:' . implode(',', config('vehicles.categories'))],
+            'proprietary' => ['required', 'string', 'in:' . implode(',', config('vehicles.proprietaries'))],
+            'status' => ['required', 'string', 'in:' . implode(',', config('vehicles.statuses'))],
             'kms' => ['required', 'integer'],
-            'fuel_type' => ['required', 'string', 'in:' . implode(',', array_keys(config('vehicles.fuel_types.assigning')))],
+            'fuel_type' => ['required', 'string', 'in:' . implode(',', config('vehicles.fuel_types.assigning'))],
+            'has_adblue' => ['required', 'boolean'],
+            'has_leasing' => ['required', 'boolean'],
+            'chassis' => ['required', 'string'],
+            'tank_capacity' => ['required', 'numeric'],
+            'fleet_card' => ['required', 'string'],
+            'registration_date' => ['required', 'date'],
+            'tireBooklet' => ['required', 'string'],
+            'tire1_brand' => ['required', 'string'],
+            'tire1_axle' => ['required', 'string'],
+            'tire2_brand' => ['required', 'string'],
+            'tire2_axle' => ['required', 'string'],
         ];
     }
 }
-
