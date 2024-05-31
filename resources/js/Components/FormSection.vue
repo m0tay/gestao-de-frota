20cdfef993ed0ceb300cc121982c6f179a309408
m0tay <mail.roi.douglas@gmail.com>
2024-05-31 15:34:32 +0100

    wip: starting to templatarize form components

---
+++ resources/js/Components/FormSection.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+
+const props = defineProps({
+    title: { type: String, required: false },
+    noBar: { type: Boolean, required: false, default: false },
+})
+</script>
+
+<template>
+    <div>
+        <h2 class="text-left font-bold">{{ props.title }}</h2>
+        <hr class="border-t border-gray-300" v-if="!props.noBar" />
+    </div>
+</template>
+
---
+++ resources/js/Components/FormTwoColumn.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col sm:flex-row gap-x-4">
+        <div class="mt-6 sm:w-half gap-y-4 flex flex-col">
+            <slot name="left" />
+        </div>
+
+        <div class="mt-6 sm:w-half gap-y-4 flex flex-col">
+            <slot name="right" />
+        </div>
+    </div>
+</template>
