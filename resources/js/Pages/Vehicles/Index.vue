ac49c9fbafc0d6f5fd3d69ec5094dc5f48a972e2
m0tay <mail.roi.douglas@gmail.com>
2024-05-31 13:57:51 +0100

    wip: fixed columns and working still

--- resources/js/Pages/Vehicles/Index.vue
+++ resources/js/Pages/Vehicles/Index.vue
@@ -1,50 +1,36 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { columns } from "@/Pages/Vehicles/columns"
-import DataTable from "@/Components/ui/data-table.vue"
+<script setup>
+import DataTable from "@/Components/ui/data-table.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { faker } from "@faker-js/faker";
+import { columns } from "@/Pages/Vehicles/columns";
+import { onMounted, ref } from 'vue';
 
-interface Vehicle {
-    licensePlate: string
-    brand: string
-    model: string
-    vin: string
-    lastInspection: Date
-}
 
-const data = ref<Vehicle[]>([])
+const props = defineProps({
+    vehicles: Array
+})
 
-async function getData(): Promise<Vehicle[]> {
-    // Fetch data from your API here.
-    return [...generateFakeVehicleData()]
+const data = ref([...props.vehicles])
+
+async function getData() {
+    return data.value.map(vehicle => ({
+        plate: vehicle.plate,
+        brand: vehicle.brand,
+        model: vehicle.model,
+        color: vehicle.color,
+        proprietary: vehicle.proprietary,
+        chassis: vehicle.chassis,
+        kms: `${vehicle.kms ?? '-'} km`,
+        category: vehicle.category,
+        active: vehicle.active ? 'Ativo' : 'Desativado',
+        private: vehicle.active ? vehicle.private ? 'Particular' : 'Comunitário' : '-',
+        fuel_type: vehicle.fuel_type,
+        status: vehicle.status,
+    }))
 }
 
 onMounted(async () => {
     data.value = await getData();
 });
-
-function generateFakeVehicleData() {
-    const data = [];
-
-    for (let i = 0; i < 50; i++) {
-        const randomLicensePlate = `${faker.helpers.replaceSymbols("??")}-${faker.helpers.replaceSymbols("##")}-${faker.helpers.replaceSymbols("??")}`;
-        const randomBrand = faker.vehicle.manufacturer();
-        const randomModel = faker.vehicle.model();
-        const randomVin = faker.vehicle.vin();
-        const randomDate = faker.date.past();
-
-        data.push({
-            licensePlate: randomLicensePlate,
-            brand: randomBrand,
-            model: randomModel,
-            vin: randomVin,
-            lastInspection: randomDate,
-        });
-    }
-
-    return data;
-}
 </script>
 
 <template>
