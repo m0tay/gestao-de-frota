f28d1647afe3a1e3595f3e42a91772e9ab11a425
m0tay <mail.roi.douglas@gmail.com>
2024-06-01 19:24:37 +0100

    feat: fleetcard create menu

--- resources/js/Pages/FleetCards/Create.vue
+++ resources/js/Pages/FleetCards/Create.vue
@@ -4,21 +4,48 @@ import FormTwoColumn from '@/Components/FormTwoColumn.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import TextInput from '@/Components/TextInput.vue';
+import SelectInput from '@/Components/Vehicles/SelectInput.vue';
+import SelectInputVehicle from '@/Components/SelectInput.vue';
 import Button from '@/Components/ui/button/Button.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
+import DateTimeInput from '@/Components/DateTimeInput.vue';
+
+const props = defineProps({
+    companies: Array,
+    types: Array,
+    vehicles: Array,
+})
 
 const form = useForm({
     company: String,
+    vehicle_id: Number,
+    type: String,
+    code: String,
+    pin: String,
+    expire_at: Date
 })
 
+const submit = () => {
+    form.post(route('fleet_cards.store'))
+}
+
 onMounted(() => {
+    form.reset()
+    form.clearErrors()
+
     form.company = null
+    form.vehicle = null
+    form.type = null
+    form.code = null
+    form.pin = null
+    form.expire_at = null
 })
 </script>
 
 <template>
+
     <Head title="Cartão Frota" />
     <AuthenticatedLayout>
         <div class="py-12">
@@ -49,16 +76,36 @@ onMounted(() => {
                             <template #left>
                                 <div>
                                     <InputLabel for="company" value="Cliente" />
-                                    <TextInput id="company" type="text" class="w-full" v-model="form.company" />
+                                    <SelectInput id="company" type="text" class="w-full" :list="companies" v-model="form.company" />
                                     <InputError :message="form.errors.company" />
+                                </div>
+                                <div>
+                                    <InputLabel for="code" value="Cart&atilde;o frota" />
+                                    <TextInput id="code" type="text" class="w-full" v-model="form.code" />
+                                    <InputError :message="form.errors.code" />
+                                </div>
+                                <div>
+                                    <InputLabel for="pin" value="PIN" />
+                                    <TextInput id="pin" type="text" class="w-full" v-model="form.pin" />
+                                    <InputError :message="form.errors.pin" />
                                 </div>
                             </template>
 
                             <template #right>
                                 <div>
-                                    <InputLabel for="company" value="Cliente" />
-                                    <TextInput id="company" type="text" class="w-full" v-model="form.company" />
-                                    <InputError :message="form.errors.company" />
+                                    <InputLabel for="type" value="Tipo" />
+                                    <SelectInput id="type" type="text" class="w-full" :list="types" v-model="form.type" />
+                                    <InputError :message="form.errors.type" />
+                                </div>
+                                <div>
+                                    <InputLabel for="expire_at" value="Validade do cartão frota" />
+                                    <DateTimeInput :min-date="true" :max-date="false" showYear id="expire_at" type="text" class="w-full" v-model="form.expire_at" />
+                                    <InputError :message="form.errors.expire_at" />
+                                </div>
+                                <div>
+                                    <InputLabel for="vehicle_id" value="Veículo associado" />
+                                    <SelectInputVehicle id="vehicle_id" type="text" class="w-full" :list="vehicles" v-model="form.vehicle_id" />
+                                    <InputError :message="form.errors.vehicle_id" />
                                 </div>
                             </template>
                         </FormTwoColumn>
@@ -66,7 +113,7 @@ onMounted(() => {
                         <div class="mt-6 flex flex-col gap-y-4 justify-end gap-x-4 sm:flex-row">
                             <Button variant="secondary">Mudei de Ideia</Button>
 
-                            <Button @click="handleSubmit">Registar</Button>
+                            <Button @click="submit">Registar</Button>
                         </div>
                     </section>
                 </div>
---
+++ resources/js/Pages/FleetCards/columns.js
@@ -0,0 +1,96 @@
+import { h } from "vue";
+import DropdownAction from "@/Components/ui/data-table-dropdown.vue";
+import { ArrowUpDown, ChevronDown } from "lucide-vue-next";
+import { Button } from "@/Components/ui/button";
+import { formatDate } from "@/lib/formatDate.js";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export const columns = [
+    {
+        accessorKey: "company",
+        header: "Empresa",
+        headerStyle: {
+            textAlign: "center",
+        },
+        cell: function (row) {
+            return h(
+                "div",
+                { class: "flex items-center" },
+                row.getValue("company")
+            );
+        },
+    },
+    {
+        accessorKey: "code",
+        header: "Código",
+        headerStyle: {
+            textAlign: "center",
+        },
+        cell: function (row) {
+            return h(
+                "div",
+                { class: "flex items-center" },
+                row.getValue("code")
+            );
+        },
+    },
+    {
+        accessorKey: "pin",
+        header: "PIN",
+        headerStyle: {
+            textAlign: "center",
+        },
+        cell: function (row) {
+            return h(
+                "div",
+                { class: "flex items-center" },
+                row.getValue("pin")
+            );
+        },
+    },
+    {
+        accessorKey: "type",
+        header: "Tipo",
+        headerStyle: {
+            textAlign: "center",
+        },
+        cell: function (row) {
+            return h(
+                "div",
+                { class: "flex items-center" },
+                row.getValue("type")
+            );
+        },
+    },
+    {
+        accessorKey: "expire_at",
+        header: "Data de Expiração",
+        headerStyle: {
+            textAlign: "center",
+        },
+        cell: function (row) {
+            return h(
+                "div",
+                { class: "flex items-center" },
+                row.getValue("expire_at")
+            );
+        },
+    },
+    {
+        accessorKey: "vehicle_id",
+        header: "Veículo",
+        headerStyle: {
+            textAlign: "center",
+        },
+        cell: function (row) {
+            const vehicleId = row.getValue("vehicle_id");
+
+            return h(
+                "div",
+                { class: "flex items-center" },
+                vehicleId ? vehicleId : "Nenhum"
+            );
+        },
+    },
+];
